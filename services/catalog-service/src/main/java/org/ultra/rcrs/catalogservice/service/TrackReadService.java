aad984fd61c0d5a16b8676ebb6472a62cbc0d33b
altroyan <altroyan@gmail.com>
2026-03-31 20:38:27 +0300

    norm

--- services/catalog-service/src/main/java/org/ultra/rcrs/catalogservice/service/TrackReadService.java
+++ services/catalog-service/src/main/java/org/ultra/rcrs/catalogservice/service/TrackReadService.java
@@ -17,7 +17,6 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class TrackReadService {
 
-    @Value
     private final TrackByIdRepository trackByIdRepository;
 
     private final AlbumByTrackRepository albumByTrackRepository;
