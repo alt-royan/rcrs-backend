6c7aeefc49eb8995718c8d6fa7fc10bd545b01d8
altroyan <alt@royan.com>
2026-07-25 02:02:42 +0300

    ну бляяяя 7

--- services/metadata-read-service/src/main/java/org/ultra/rcrs/metadata/config/SecurityConfig.java
+++ services/metadata-read-service/src/main/java/org/ultra/rcrs/metadata/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
 
         reactiveJwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
             Flux<GrantedAuthority> authorities = Flux.fromIterable(jwtGrantedAuthoritiesConverter.convert(jwt));
-            Flux<String> roles = Mono.justOrEmpty(jwt.getClaimAsStringList("resource_access.rcrs.roles"))
+            Flux<String> roles = Mono.justOrEmpty(jwt.getClaimAsStringList("rcrs-roles"))
                     .flatMapMany(Flux::fromIterable);
 
             return Flux.concat(authorities, roles
